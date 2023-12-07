85d58990c4574d93f44cb2a67fdf75f2aa962505
ValentinVaninetti <valenga20@gmail.com>
2023-12-07 13:57:58 -0300

    Deleted Dist folder and started to ignore it

--- dist/entities/university/Department.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const departmentSchema = new mongoose_1.Schema({
-    type: { type: String, required: true },
-    majorList: [{ type: mongoose_1.Schema.ObjectId, ref: "Major" }]
-});
-const Department = (0, mongoose_1.model)('Department', departmentSchema);
-exports.default = Department;
--- dist/entities/university/Major.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const majorSchema = new mongoose_1.Schema({
-    name: { type: String, required: true },
-    courseList: [{ type: mongoose_1.Schema.ObjectId, ref: "Course" }]
-});
-const Major = (0, mongoose_1.model)('Major', majorSchema);
-exports.default = Major;
